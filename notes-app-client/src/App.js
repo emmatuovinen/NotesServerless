92d7d00201af33e2386a15bcfb7be73bc8749616
emmaTu <emma.n.tuovinen@gmail.com>
2019-01-15 14:08:56 +0200

    first commit, adding projects to git

---
+++ notes-app-client/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Navbar, NavItem, Nav } from "react-bootstrap";
+import "./App.css";
+import Routes from "./Routes";
+import { LinkContainer } from "react-router-bootstrap";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">My Notes</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <LinkContainer to="/signup">
+                <NavItem>Sign Up</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <NavItem>Login</NavItem>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ notes-app-client/src/containers/NotFound.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import "./NotFound.css";
+
+export default () =>
+    <div className="NotFound">
+        <h3>Sorry, nothing to see here!</h3>
+    </div>;
